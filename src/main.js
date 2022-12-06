8aa145a7ba0cdeefa76021033e4bf0c2f5c3f1c1
quang4102001 <90362115+quang4102001@users.noreply.github.com>
2022-12-06 19:32:37 +0700

    lan4

--- src/main.js
+++ src/main.js
@@ -1,53 +1,62 @@
 const desktopHeaderNavArray = [
     {
+        id:1,
         iconClass: "fa-solid fa-house", 
         title:"home",
         to:"#home",
         active:true
     },
     {
+        id:2,
         iconClass: "fa-solid fa-address-card", 
         title:"about",
         to:"#about",
         active:false
     },
     {
+        id:3,
         iconClass: "fa-solid fa-briefcase", 
         title:"services",
         to:"#services",
         active:false
     },
     {
+        id:4,
         iconClass: "fa-solid fa-graduation-cap", 
         title:"experience",
         to:"#experience",
         active:false
     },
     {
+        id:5,
         iconClass: "fa-solid fa-layer-group", 
         title:"works",
         to:"#works",
         active:false
     },
     {
+        id:6,
         iconClass:"fa-solid fa-coins", 
         title:"price",
         to:"#price",
         active:false
     },
     {
+        id:7,
         iconClass:"fa-solid fa-comment", 
         title:"testimonial",
         to:"#testimonial",
         active:false
     },
     {
+        id:8,
         iconClass: "fa-solid fa-pen-to-square", 
         title:"blog",
         to:"#blog",
         active:false
     },
     {
+        id:9,
         iconClass: "fa-solid fa-comments", 
         title:"contact",
         to:"#contact",
@@ -56,6 +65,88 @@ const desktopHeaderNavArray = [
 ]
 const homeDescriptionArray = ["Front-end Developer", "UI/UX designer", "Photography lover"]
 const homeIconClassArray = ["fa-brands fa-instagram", "fa-brands fa-twitter", "fa-brands fa-behance", "fa-solid fa-basketball", "fa-brands fa-pinterest-p"]
+const homeLayerArray = [
+    {
+        id:1,
+        iconElement: '<svg width="27" height="29" data-depth="0.3" class="layer p1" xmlns="http://www.w3.org/2000/svg" style="left: 10%;top: 10%; transform-style: preserve-3d; backface-visibility: hidden; position: relative; display: block;"><path d="M21.15625.60099c4.37954 3.67487 6.46544 9.40612 5.47254 15.03526-.9929 5.62915-4.91339 10.30141-10.2846 12.25672-5.37122 1.9553-11.3776.89631-15.75715-2.77856l2.05692-2.45134c3.50315 2.93948 8.3087 3.78663 12.60572 2.22284 4.297-1.5638 7.43381-5.30209 8.22768-9.80537.79387-4.50328-.8749-9.08872-4.37803-12.02821L21.15625.60099z" fill="#FFD15C" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:2,
+        iconElement: '<svg width="26" height="26" data-depth="0.2" class="layer p2" xmlns="http://www.w3.org/2000/svg" style="left: 25%;top: 30%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M13 3.3541L2.42705 24.5h21.1459L13 3.3541z" stroke="#FF4C60" stroke-width="3" fill="none" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:3,
+        iconElement: '<svg width="30" height="25" data-depth="0.3" class="layer p3" xmlns="http://www.w3.org/2000/svg" style="left: 15%;top: 30%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M.1436 8.9282C3.00213 3.97706 8.2841.92763 14.00013.92796c5.71605.00032 10.9981 3.04992 13.85641 8 2.8583 4.95007 2.8584 11.0491-.00014 16.00024l-2.77128-1.6c2.28651-3.96036 2.28631-8.84002.00011-12.8002-2.2862-3.96017-6.5124-6.40017-11.08513-6.4-4.57271.00018-8.79872 2.43984-11.08524 6.4002l-2.77128-1.6z" fill="#44D7B6" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:4,
+        iconElement: '<svg width="15" height="23" data-depth="0.6" class="layer p4" xmlns="http://www.w3.org/2000/svg" style="left: 10%;bottom: 10%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><rect transform="rotate(30 9.86603 10.13397)" x="7" width="3" height="25" rx="1.5" fill="#FFD15C" fill-rule="evenodd"></rect></svg>'
+    },
+    {
+        id:5,
+        iconElement: '<svg width="15" height="23" data-depth="0.2" class="layer p5" xmlns="http://www.w3.org/2000/svg" style="left: 45%;top: 10%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><rect transform="rotate(30 9.86603 10.13397)" x="7" width="3" height="25" rx="1.5" fill="#6C6CE5" fill-rule="evenodd"></rect></svg>'
+    },
+    {
+        id:6,
+        iconElement: '<svg width="49" height="17" data-depth="0.5" class="layer p6" xmlns="http://www.w3.org/2000/svg" style="left: 40%;bottom: 10%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><g fill="#FF4C60" fill-rule="evenodd"><path d="M.5 16.5c0-5.71709 2.3825-10.99895 6.25-13.8567 3.8675-2.85774 8.6325-2.85774 12.5 0C23.1175 5.50106 25.5 10.78292 25.5 16.5H23c0-4.57303-1.90625-8.79884-5-11.08535-3.09375-2.28652-6.90625-2.28652-10 0C4.90625 7.70116 3 11.92697 3 16.5H.5z"></path><path d="M23.5 16.5c0-5.71709 2.3825-10.99895 6.25-13.8567 3.8675-2.85774 8.6325-2.85774 12.5 0C46.1175 5.50106 48.5 10.78292 48.5 16.5H46c0-4.57303-1.90625-8.79884-5-11.08535-3.09375-2.28652-6.90625-2.28652-10 0-3.09375 2.28651-5 6.51232-5 11.08535h-2.5z"></path></g></svg>'
+    },
+    {
+        id:7,
+        iconElement: '<svg width="26" height="26" data-depth="0.4" class="layer p7" xmlns="http://www.w3.org/2000/svg" style="left: 70%%;top: 20%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M13 22.6459L2.42705 1.5h21.1459L13 22.6459z" stroke="#FFD15C" stroke-width="3" fill="none" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:8,
+        iconElement: '<svg width="19" height="21" data-depth="0.3" class="layer p8" xmlns="http://www.w3.org/2000/svg" style="left: 90%;top: 10%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><rect transform="rotate(-40 6.25252 10.12626)" x="7" width="3" height="25" rx="1.5" fill="#6C6CE5" fill-rule="evenodd"></rect></svg>'
+    },
+    {
+        id:9,
+        iconElement: '<svg width="30" height="25" data-depth="0.3" data-depth-y="-1.30" class="layer p9" xmlns="http://www.w3.org/2000/svg" style="left: 80%;top: 45%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M29.8564 16.0718c-2.85854 4.95114-8.1405 8.00057-13.85654 8.00024-5.71605-.00032-10.9981-3.04992-13.85641-8-2.8583-4.95007-2.8584-11.0491.00014-16.00024l2.77128 1.6c-2.28651 3.96036-2.28631 8.84002-.00011 12.8002 2.2862 3.96017 6.5124 6.40017 11.08513 6.4 4.57271-.00018 8.79872-2.43984 11.08524-6.4002l2.77128 1.6z" fill="#6C6CE5" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:10,
+        iconElement: '<svg width="47" height="29" data-depth="0.2" class="layer p10" xmlns="http://www.w3.org/2000/svg" style="left: 90%;top: 90%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><g fill="#44D7B6" fill-rule="evenodd"><path d="M46.78878 17.19094c-1.95535 5.3723-6.00068 9.52077-10.61234 10.8834-4.61167 1.36265-9.0893-.26708-11.74616-4.27524-2.65686-4.00817-3.08917-9.78636-1.13381-15.15866l2.34923.85505c-1.56407 4.29724-1.2181 8.92018.90705 12.12693 2.12514 3.20674 5.70772 4.5107 9.39692 3.4202 3.68921-1.0905 6.92581-4.40949 8.48988-8.70673l2.34923.85505z"></path><path d="M25.17585 9.32448c-1.95535 5.3723-6.00068 9.52077-10.61234 10.8834-4.61167 1.36264-9.0893-.26708-11.74616-4.27525C.16049 11.92447-.27182 6.14628 1.68354.77398l2.34923.85505c-1.56407 4.29724-1.2181 8.92018.90705 12.12692 2.12514 3.20675 5.70772 4.5107 9.39692 3.4202 3.68921-1.0905 6.92581-4.40948 8.48988-8.70672l2.34923.85505z"></path></g></svg>'
+    },
+    {
+        id:11,
+        iconElement: '<svg width="33" height="20" data-depth="0.5" class="layer p11" xmlns="http://www.w3.org/2000/svg" style="left: 70%;top: 80%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M32.36774.34317c.99276 5.63023-1.09332 11.3614-5.47227 15.03536-4.37895 3.67396-10.3855 4.73307-15.75693 2.77837C5.76711 16.2022 1.84665 11.53014.8539 5.8999l3.15139-.55567c.7941 4.50356 3.93083 8.24147 8.22772 9.8056 4.29688 1.56413 9.10275.71673 12.60554-2.2227C28.34133 9.98771 30.01045 5.4024 29.21635.89884l3.15139-.55567z" fill="#FFD15C" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:12,
+        iconElement: '<svg width="30" height="25" data-depth="0.3" class="layer p12" xmlns="http://www.w3.org/2000/svg" style="left: 55%;top: 5%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M.1436 8.9282C3.00213 3.97706 8.2841.92763 14.00013.92796c5.71605.00032 10.9981 3.04992 13.85641 8 2.8583 4.95007 2.8584 11.0491-.00014 16.00024l-2.77128-1.6c2.28651-3.96036 2.28631-8.84002.00011-12.8002-2.2862-3.96017-6.5124-6.40017-11.08513-6.4-4.57271.00018-8.79872 2.43984-11.08524 6.4002l-2.77128-1.6z" fill="#44D7B6" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:13,
+        iconElement: '<svg width="15" height="23" data-depth="0.6" class="layer p13" xmlns="http://www.w3.org/2000/svg" style="left: 10%;bottom: 10%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><rect transform="rotate(30 9.86603 10.13397)" x="7" width="3" height="25" rx="1.5" fill="#FFD15C" fill-rule="evenodd"></rect></svg>'
+    },
+    {
+        id:14,
+        iconElement: '<svg width="15" height="23" data-depth="0.2" class="layer p14" xmlns="http://www.w3.org/2000/svg" style="left: 25%;top: 60%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><rect transform="rotate(30 9.86603 10.13397)" x="7" width="3" height="25" rx="1.5" fill="#6C6CE5" fill-rule="evenodd"></rect></svg>'
+    },
+    {
+        id:15,
+        iconElement: '<svg width="49" height="17" data-depth="0.5" class="layer p15" xmlns="http://www.w3.org/2000/svg" style="left: 10%;bottom: 60%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><g fill="#FF4C60" fill-rule="evenodd"><path d="M.5 16.5c0-5.71709 2.3825-10.99895 6.25-13.8567 3.8675-2.85774 8.6325-2.85774 12.5 0C23.1175 5.50106 25.5 10.78292 25.5 16.5H23c0-4.57303-1.90625-8.79884-5-11.08535-3.09375-2.28652-6.90625-2.28652-10 0C4.90625 7.70116 3 11.92697 3 16.5H.5z"></path><path d="M23.5 16.5c0-5.71709 2.3825-10.99895 6.25-13.8567 3.8675-2.85774 8.6325-2.85774 12.5 0C46.1175 5.50106 48.5 10.78292 48.5 16.5H46c0-4.57303-1.90625-8.79884-5-11.08535-3.09375-2.28652-6.90625-2.28652-10 0-3.09375 2.28651-5 6.51232-5 11.08535h-2.5z"></path></g></svg>'
+    },
+    {
+        id:16,
+        iconElement: '<svg width="26" height="26" data-depth="0.4" class="layer p16" xmlns="http://www.w3.org/2000/svg" style="left: 70%%;top: 70%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M13 22.6459L2.42705 1.5h21.1459L13 22.6459z" stroke="#FFD15C" stroke-width="3" fill="none" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:17,
+        iconElement: '<svg width="19" height="21" data-depth="0.3" class="layer p17" xmlns="http://www.w3.org/2000/svg" style="left: 90%;top: 90%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><rect transform="rotate(-40 6.25252 10.12626)" x="7" width="3" height="25" rx="1.5" fill="#6C6CE5" fill-rule="evenodd"></rect></svg>'
+    },
+    {
+        id:18,
+        iconElement: '<svg width="30" height="25" data-depth="0.3" data-depth-y="-1.30" class="layer p18" xmlns="http://www.w3.org/2000/svg" style="left: 40%;top: 75%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M29.8564 16.0718c-2.85854 4.95114-8.1405 8.00057-13.85654 8.00024-5.71605-.00032-10.9981-3.04992-13.85641-8-2.8583-4.95007-2.8584-11.0491.00014-16.00024l2.77128 1.6c-2.28651 3.96036-2.28631 8.84002-.00011 12.8002 2.2862 3.96017 6.5124 6.40017 11.08513 6.4 4.57271-.00018 8.79872-2.43984 11.08524-6.4002l2.77128 1.6z" fill="#6C6CE5" fill-rule="evenodd"></path></svg>'
+    },
+    {
+        id:19,
+        iconElement: '<svg width="47" height="29" data-depth="0.2" class="layer p19" xmlns="http://www.w3.org/2000/svg" style="left: 30%;top: 60%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><g fill="#44D7B6" fill-rule="evenodd"><path d="M46.78878 17.19094c-1.95535 5.3723-6.00068 9.52077-10.61234 10.8834-4.61167 1.36265-9.0893-.26708-11.74616-4.27524-2.65686-4.00817-3.08917-9.78636-1.13381-15.15866l2.34923.85505c-1.56407 4.29724-1.2181 8.92018.90705 12.12693 2.12514 3.20674 5.70772 4.5107 9.39692 3.4202 3.68921-1.0905 6.92581-4.40949 8.48988-8.70673l2.34923.85505z"></path><path d="M25.17585 9.32448c-1.95535 5.3723-6.00068 9.52077-10.61234 10.8834-4.61167 1.36264-9.0893-.26708-11.74616-4.27525C.16049 11.92447-.27182 6.14628 1.68354.77398l2.34923.85505c-1.56407 4.29724-1.2181 8.92018.90705 12.12692 2.12514 3.20675 5.70772 4.5107 9.39692 3.4202 3.68921-1.0905 6.92581-4.40948 8.48988-8.70672l2.34923.85505z"></path></g></svg>'
+    },
+    {
+        id:20,
+        iconElement: '<svg width="33" height="20" data-depth="0.5" class="layer p20" xmlns="http://www.w3.org/2000/svg" style="left: 50%;top: 80%; transform-style: preserve-3d; backface-visibility: hidden; position: absolute; display: block;"><path d="M32.36774.34317c.99276 5.63023-1.09332 11.3614-5.47227 15.03536-4.37895 3.67396-10.3855 4.73307-15.75693 2.77837C5.76711 16.2022 1.84665 11.53014.8539 5.8999l3.15139-.55567c.7941 4.50356 3.93083 8.24147 8.22772 9.8056 4.29688 1.56413 9.10275.71673 12.60554-2.2227C28.34133 9.98771 30.01045 5.4024 29.21635.89884l3.15139-.55567z" fill="#FFD15C" fill-rule="evenodd"></path></svg>'
+    },
+]
 const aboutInfoSkillArray = [
     {
         id:1,
@@ -348,7 +439,6 @@ const blogItemArray = [
 ]
 
 
-const main = document.querySelector(".main")
 const desktopHeaderNavList = document.querySelector(".desktop-header__nav-list")
 const homeDescription = document.querySelector(".home__description__random")
 const homeIconList = document.querySelector(".home__icon-list")
@@ -363,20 +453,53 @@ const testimonialClientImgList = document.querySelector(".testimonial .testimoni
 const blogItemList = document.querySelector(".blog .blog-list .row")
 
 
-// Hiển thị nút scroll to đầu trang
+const main = document.querySelector(".main")
+const sectionArray = Array.from(document.querySelectorAll("section[id]"))
+const home = document.querySelector("#home")
+// const about = document.querySelector("#about")
+// const services = document.querySelector("#services")
+// const experience = document.querySelector("#experience")
+// const works = document.querySelector("#works")
+// const price = document.querySelector("#price")
+// const testimonial = document.querySelector("#testimonial")
+// const blog = document.querySelector("#blog")
+// const contact = document.querySelector("#contact")
+
+// Hiển thị nút scroll to đầu trang và Xử lý nav list header desktop
 const btnScrollToTop = document.querySelector(".scroll-to-top")
+const sectionArrayNew = sectionArray.slice().reverse()
+main.addEventListener("scroll", () => {
+    main.scrollTop >= 500 
+    ? btnScrollToTop.style.display = "flex"
+    : btnScrollToTop.style.display = "none"
+    sectionArrayNew.every(section => {
+        if(main.scrollTop >= section.offsetTop){
+            const navActiveId = sectionArray.indexOf(section)+1
+            desktopHeaderNavArray.forEach(desktopHeaderNav => {
+                desktopHeaderNav.id === navActiveId 
+                ? desktopHeaderNav.active=true
+                : desktopHeaderNav.active=false
+            })
+            loadDesktopHeaderNavList()
+            return false
+        }
+        return true
+    })
+})
 
 
 //Hiển thị list nav header
-const desktopHeaderNavListHtml  
-= desktopHeaderNavArray.map(desktopHeaderNav => 
-    `<li class="desktop-header__nav-item">
-        <a href="${desktopHeaderNav.to}" class="nav-link header__nav__icon-${desktopHeaderNav.title}${desktopHeaderNav.active ? " active" : ""}">
-            <i class="${desktopHeaderNav.iconClass}"></i>
-            ${desktopHeaderNav.title}
-        </a>
-    </li>`).toString().replaceAll(">,<","><")
-desktopHeaderNavList.innerHTML = desktopHeaderNavListHtml
+function loadDesktopHeaderNavList() {
+    const desktopHeaderNavListHtml = desktopHeaderNavArray.map(desktopHeaderNav => 
+        `<li class="desktop-header__nav-item">
+            <a href="${desktopHeaderNav.to}" class="nav-link header__nav__icon-${desktopHeaderNav.title}${desktopHeaderNav.active ? " active" : ""}">
+                <i class="${desktopHeaderNav.iconClass}"></i>
+                ${desktopHeaderNav.title}
+            </a>
+        </li>`).toString().replaceAll(">,<","><")
+    desktopHeaderNavList.innerHTML = desktopHeaderNavListHtml
+}
+loadDesktopHeaderNavList()
 
 
 //Cho home description chạy tự động
@@ -392,7 +515,7 @@ const homeIconListHtml
 homeIconList.innerHTML = homeIconListHtml
 
 
-// Hiển thị list skill trong sction about
+// Hiển thị list skill trong section about
 const aboutInfoSkillHtml 
 = aboutInfoSkillArray.map(aboutInfoSkill => `<div class="skill-item">
         <div class="skill-item__info">
@@ -516,15 +639,15 @@ loadTestimonialItemList()
 
 
 // Chạy slide testimonial
-let index = 0
+let index = 3
 setInterval(() => {
     let indexNew
     do{
         indexNew = Math.floor(Math.random()*testimonialItemArray.length)
-    }while(Math.abs(indexNew - index) > 2)
-        index = indexNew
-        loadTestimonialItemList()
-        testimonialItemList.style.transform = `translateX(calc(-700px * ${index}))`
+    }while(Math.abs(indexNew - index) > 2 || indexNew == index)
+    index = indexNew    
+    loadTestimonialItemList()
+    testimonialItemList.style.transform = `translateX(calc(-700px * ${index}))`
 },3000)
 
 
@@ -567,7 +690,7 @@ btnContactForm.addEventListener("click", (e) => {
     validate()
 })
 inputContactFormArray.forEach(inputContactForm => {
-    inputContactForm.addEventListener("focus", () => {
+    inputContactForm.addEventListener("blur", () => {
         setInterval(validate())
     })
 })
@@ -580,5 +703,33 @@ function validate() {
         ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(inputContactForm.value) ? showErr.innerHTML="" : showErr.innerHTML=`${nameInputContactFormNew} is required.` 
         : !!inputContactForm.value.trim() == true ? showErr.innerHTML="" : showErr.innerHTML=`${nameInputContactFormNew} is required.`
     })
-    
 }
+
+
+
+// Hiển thị các layer và Chuyển động các layer trong section home
+const homeLayerHtml = homeLayerArray.forEach(homeLayer => {
+    home.innerHTML += `${homeLayer.iconElement}`
+})
+
+setTimeout(() => {
+    const homeLayerArr = Array.from(home.querySelectorAll(".layer"))
+    homeLayerArr.map(homeLayer => {
+        let x = Math.floor(Math.random() * home.offsetWidth)/50
+        let y = Math.floor(Math.random() * home.offsetHeight)/50
+        homeLayer.style.transform = `translate3d(${x}px,${y}px,0px)`
+    })
+})
+
+setInterval(() => {
+    const homeLayerArr = Array.from(home.querySelectorAll(".layer"))
+    homeLayerArr.map(homeLayer => {
+        let x = Math.floor(Math.random() * home.offsetWidth)/10
+        let y = Math.floor(Math.random() * home.offsetHeight)/10
+        homeLayer.style.transform = `translate3d(${x}px,${y}px,0px)`
+    })
+}, 3000)
+
+
+
+     
